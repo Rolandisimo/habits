901c7a95605ff32eed0b1cde94fd74973be7f237
Rolands Jegorovs <rolandjegorov@gmail.com>
2018-01-02 15:55:20 +0100

    Add filter options, add toggling animation

--- src/components/filter-bar/FilterBar.tsx
+++ src/components/filter-bar/FilterBar.tsx
@@ -1,20 +1,59 @@
 import React  from "react";
 import {
     View,
+    TouchableOpacity,
+    TouchableWithoutFeedback,
     Text,
+    Animated,
+    GestureResponderEvent,
 } from "react-native";
 import FAIcon from "react-native-vector-icons/FontAwesome";
-import { styles } from "./styles";
 import { colors } from '../consts';
+import {
+    styles,
+    filterOptionsContainer,
+} from "./styles";
 
 export interface FilterBarProps {
 }
 
-export class FilterBar extends React.PureComponent<FilterBarProps, {}> {
-    render() {
+export interface FilterState {
+    isFilterOpen: boolean;
+    fadeAnimationValue: Animated.Value;
+    topPositionAnimationValue: Animated.Value;
+}
+
+export class FilterBar extends React.PureComponent<FilterBarProps, FilterState> {
+    public constructor(props: FilterBarProps) {
+        super(props);
+
+        this.onOpenFilterToggle = this.onOpenFilterToggle.bind(this);
+        this.onOptionPress = this.onOptionPress.bind(this);
+
+        this.state = {
+            isFilterOpen: false,
+            fadeAnimationValue: new Animated.Value(0),
+            topPositionAnimationValue: new Animated.Value(0),
+        }
+    }
+    public render() {
+        const filterOptionsContainerStyles = [
+            {
+                ...filterOptionsContainer,
+                opacity: this.state.fadeAnimationValue,
+                transform: [
+                    {scaleY: this.state.topPositionAnimationValue},
+                    {perspective: 1000}, // FROM SPEC: without this line this Animation will not render on Android while working fine on iOS
+                ],
+            },
+        ];
+
         return (
             <View style={styles.container}>
-                <View style={styles.innerContainer}>
+                <TouchableOpacity
+                    style={styles.innerContainer}
+                    onPress={this.onOpenFilterToggle}
+                >
                     <Text style={styles.filterLabel}>
                         Showing: <Text style={styles.filterMode}>All</Text>
                     </Text>
@@ -22,9 +61,80 @@ export class FilterBar extends React.PureComponent<FilterBarProps, {}> {
                         name="caret-down"
                         color={colors.successColor}
                         size={20}
+                        style={this.state.isFilterOpen ? { transform: [{ rotate: "-90deg" }]} : undefined}
                     />
-                </View>
+                </TouchableOpacity>
+                <Animated.View
+                    pointerEvents={this.state.isFilterOpen ? "all" : "none"}
+                    style={filterOptionsContainerStyles}
+                >
+                    <TouchableOpacity
+                        style={styles.filterOption}
+                        onPress={this.onOptionPress}
+                    >
+                        <Text style={styles.filterOptionLabel}>All</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.filterOption}
+                        onPress={this.onOptionPress}
+                    >
+                        <Text style={styles.filterOptionLabel}>Done</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.filterOption}
+                        onPress={this.onOptionPress}
+                    >
+                         <Text style={styles.filterOptionLabel}>Not Done</Text>
+                    </TouchableOpacity>
+                </Animated.View>
             </View>
         );
+    }
+    private onOpenFilterToggle() {
+        this.setState({
+            isFilterOpen: !this.state.isFilterOpen,
+        }, this.toggleAnimationFade);
+    }
+    private toggleAnimationFade() {
+        if (this.state.isFilterOpen) {
+            // Opacity animation
+            Animated.timing(
+                this.state.fadeAnimationValue,
+                {
+                    toValue: 1,
+                    duration: 200,
+                }
+            ).start();  
+
+            // Top animation
+            Animated.timing(
+                this.state.topPositionAnimationValue,
+                {
+                    toValue: 1,
+                    duration: 200,
+                }
+            ).start();  
+        } else {
+            // Opacity animation
+            Animated.timing(
+                this.state.fadeAnimationValue,
+                {
+                    toValue: 0,
+                    duration: 200,
+                }
+            ).start();
+
+            // Top animation
+            Animated.timing(
+                this.state.topPositionAnimationValue,
+                {
+                    toValue: 0,
+                    duration: 200,
+                }
+            ).start();  
+        }
+    }
+    private onOptionPress(event: GestureResponderEvent) {
+        // Get inner text and set the filter
     }
 }
--- src/components/filter-bar/styles.ts
+++ src/components/filter-bar/styles.ts
@@ -1,16 +1,18 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
 import {
     colors,
     sizes,
     BASE_FONT_SIZE,
 } from "../../components/consts";
 
+
+export const filterBarHeight = 40;
 export const styles = StyleSheet.create({
     container: {
         padding: 10,
         backgroundColor: "#fff",
         flex: 1,
-        flexBasis: 40,
+        flexBasis: filterBarHeight,
         flexGrow: 0,
         alignItems: "center",
         justifyContent: "center",
@@ -24,6 +26,8 @@ export const styles = StyleSheet.create({
         alignItems: "center",
         justifyContent: "center",
         flexDirection: "row",
+        overflow: "hidden",
+        
     },
     filterLabel: {
         fontSize: BASE_FONT_SIZE - 2,
@@ -33,5 +37,31 @@ export const styles = StyleSheet.create({
         color: colors.dangerColor,
         fontWeight: "600",
     },
+    filterOption: {
+        borderWidth: 1,
+        borderColor: colors.mainColor,
+        borderRadius: 4,
+        padding: 10,
+        flex: 1,
+    },
+    filterOptionLabel: {
+        textAlign: "center",
+    }
 });
+export const filterOptionsContainer = {
+    borderWidth: 1,
+    backgroundColor: "rgba(255, 255, 255, .9)",
+    borderColor: colors.lightBorderColor,
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+    opacity: 0,
+    flexShrink: 0,
+    flexGrow: 1,
+    width: Dimensions.get("window").width,
+    top: filterBarHeight,
+    position: "absolute",
+    ...sizes.shadowStyles,
+};
  
